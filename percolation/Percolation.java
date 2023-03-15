739de908d470c5838c4018cb675d88e5b688c580
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2023-03-15 23:46:06 +1100

    Rename dim to size

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -4,7 +4,7 @@ public class Percolation {
     private final int[][] grid; // 2d array for referencing grid index
     private boolean[] state; // true if site is open
     private final WeightedQuickUnionUF gridUF;
-    private final int dim;
+    private final int size;
     private int openSites;
     private final int topVirtualSite;
     private final int botVirtualSite;
@@ -19,14 +19,14 @@ public class Percolation {
     private void checkGridEdgeUnion(int row, int col) {
         if (col == 1) {
             gridUF.union(grid[row][col], grid[row][col + 1]);
-        } else if (col == dim) {
+        } else if (col == size) {
             gridUF.union(grid[row][col], grid[row][col - 1]);
         }
     }
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-        dim = n;
+        size = n;
         gridUF = new WeightedQuickUnionUF(n * n + 2);
         grid = new int[n][n];
         state = new boolean[n * n + 2];
@@ -57,13 +57,13 @@ public class Percolation {
             state[grid[row - 1][col - 1]] = true;
             openSites++;
 
-            if (row != dim && isOpen(row + 1, col)) {
+            if (row != size && isOpen(row + 1, col)) {
                 gridUF.union(grid[row - 1][col - 1], grid[row][col - 1]);
             }
             if (row != 1 && isOpen(row - 1, col)) {
                 gridUF.union(grid[row - 1][col - 1], grid[row - 2][col - 1]);
             }
-            if (col != dim && isOpen(row, col + 1)) {
+            if (col != size && isOpen(row, col + 1)) {
                 gridUF.union(grid[row - 1][col - 1], grid[row - 1][col]);
             }
             if (col != 1 && isOpen(row, col - 1)) {
@@ -97,23 +97,23 @@ public class Percolation {
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation myPercolation = new Percolation(5);
-        myPercolation.open(1, 1);
-        myPercolation.open(1, 4);
-        myPercolation.open(2, 1);
-        myPercolation.open(2, 3);
-        myPercolation.open(2, 4);
-        myPercolation.open(3, 1);
-        myPercolation.open(3, 3);
-        myPercolation.open(3, 5);
-        myPercolation.open(4, 3);
-        myPercolation.open(4, 4);
-        myPercolation.open(5, 1);
-        myPercolation.open(5, 4);
-        System.out.println(myPercolation.numberOfOpenSites());
-        System.out.println(myPercolation.isFull(2, 1));
-        System.out.println(myPercolation.isFull(3, 1));
-        System.out.println(myPercolation.isFull(3, 5));
-        System.out.println(myPercolation.percolates());
+//        Percolation myPercolation = new Percolation(5);
+//        myPercolation.open(1, 1);
+//        myPercolation.open(1, 4);
+//        myPercolation.open(2, 1);
+//        myPercolation.open(2, 3);
+//        myPercolation.open(2, 4);
+//        myPercolation.open(3, 1);
+//        myPercolation.open(3, 3);
+//        myPercolation.open(3, 5);
+//        myPercolation.open(4, 3);
+//        myPercolation.open(4, 4);
+//        myPercolation.open(5, 1);
+//        myPercolation.open(5, 4);
+//        System.out.println(myPercolation.numberOfOpenSites());
+//        System.out.println(myPercolation.isFull(2, 1));
+//        System.out.println(myPercolation.isFull(3, 1));
+//        System.out.println(myPercolation.isFull(3, 5));
+//        System.out.println(myPercolation.percolates());
     }
 }
