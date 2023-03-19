1801ee4ee12e41410a8b0c2c0cc1bc52c0d2010b
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2023-03-20 10:27:38 +1100

    Add exceptions required in assignment spec

--- queues/Deque.java
+++ queues/Deque.java
@@ -5,34 +5,80 @@
  **************************************************************************** */
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
+    private Node first, last = null;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
 
     // construct an empty deque
-    public Deque()
+    public Deque() {
+
+    }
 
     // is the deque empty?
-    public boolean isEmpty()
+    public boolean isEmpty() {
+        return first == null;
+    }
 
     // return the number of items on the deque
-    public int size()
+    public int size() {
+        return 0;
+    }
 
     // add the item to the front
-    public void addFirst(Item item)
+    public void addFirst(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+    }
 
     // add the item to the back
-    public void addLast(Item item)
+    public void addLast(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+    }
 
     // remove and return the item from the front
-    public Item removeFirst()
+    public Item removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException();
+        return null;
+    }
 
     // remove and return the item from the back
-    public Item removeLast()
+    public Item removeLast() {
+        if (isEmpty()) throw new NoSuchElementException();
+        return null;
+    }
 
     // return an iterator over items in order from front to back
-    public Iterator<Item> iterator()
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            if (current == null) throw new NoSuchElementException();
+            return item;
+        }
+
+    }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+    }
 
 }
