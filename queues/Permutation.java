514ebef89146c86f6fd1be36c4773a8a3fbb7678
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2023-03-21 18:02:41 +1100

    Refresh folder

---
+++ queues/Permutation.java
@@ -0,0 +1,13 @@
+/* *****************************************************************************
+ *  Name: Kevin Chen
+ *  Date: 21/03/2023
+ *  Description: that takes an integer k as a command-line argument; reads a
+ *  sequence of strings from standard input using StdIn.readString(); and prints
+ *  exactly k of them, uniformly at random.
+ **************************************************************************** */
+
+public class Permutation {
+    public static void main(String[] args) {
+
+    }
+}
--- queues/RandomizedQueue.java
+++ queues/RandomizedQueue.java
@@ -1,35 +1,85 @@
 /* *****************************************************************************
  *  Name: Kevin Chen
- *  Date: 19/03/2023
+ *  Date: 21/03/2023
  *  Description: Similar to a queue but removal is uniformly random
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Node first, last;
+    private int numNodes;
+    private Item[] q;
+
+    private class Node {
+        Item item;
+        Node next;
+        Node prev;
+    }
 
     // construct an empty randomized queue
-    public RandomizedQueue()
+    public RandomizedQueue() {
+        first = null;
+        last = null;
+        numNodes = 0;
+    }
 
     // is the randomized queue empty?
-    public boolean isEmpty()
+    public boolean isEmpty() {
+        return numNodes == 0;
+    }
 
     // return the number of items on the randomized queue
-    public int size()
+    public int size() {
+        return numNodes;
+    }
 
     // add the item
-    public void enqueue(Item item)
+    public void enqueue(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+    }
 
     // remove and return a random item
-    public Item dequeue()
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException();
+        return null;
+    }
 
     // return a random item (but do not remove it)
-    public Item sample()
+    public Item sample() {
+        if (isEmpty()) throw new NoSuchElementException();
+        return null;
+    }
 
     // return an independent iterator over items in random order
-    public Iterator<Item> iterator()
+    public Iterator<Item> iterator() {
+        return new RandomIterator();
+    }
+
+    private class RandomIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            if (current == null) throw new NoSuchElementException();
+            Item item = current.item;
+            return item;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        StdRandom.uniform();
+    }
 
 }
